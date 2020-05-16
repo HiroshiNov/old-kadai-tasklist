a5362fd8dd2bb0d3db7724b3c09354ada094ce17
hiroshi.m <hiroshi.m.nov@gmail.com>
2020-05-16 11:14:08 +0000

    modified routes for session

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # post 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "tasks#index"
+
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'login', to: "sessions#destroy"
+
   get 'signup', to: 'users#new'
   resources :tasks
   resources :users , only: [:index,:create,:show]
