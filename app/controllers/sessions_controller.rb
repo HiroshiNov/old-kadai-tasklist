f1d688a7aeb090aacbae5b8ce55a422a7077d321
hiroshi.m <hiroshi.m.nov@gmail.com>
2020-05-16 11:58:27 +0000

    logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+      sessin[:user_id] = nil
+      flash[:success] = "ログアウトしました"
+      redirect_to root_url
   end
   
   private
